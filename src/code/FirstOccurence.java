3b888e6a4b4e2faa0bda1bad9b803b5da948cd81
Marco Tosini <mark.tosini@gmail.com>
2024-11-03 10:31:22 +0100

    first occurence

---
+++ src/code/FirstOccurence.java
@@ -0,0 +1,47 @@
+package code;
+
+public class FirstOccurence {
+
+	public static void main(String[] args) {
+		FirstOccurence firstOccurence = new FirstOccurence();
+		System.out.println(firstOccurence.strStr("sadbutsad", "sad"));
+		System.out.println(firstOccurence.strStr("leetcode", "leeto"));
+		System.out.println(firstOccurence.strStr("aleetocode", "leeto"));
+		System.out.println(firstOccurence.strStr("", ""));
+		System.out.println(firstOccurence.strStr("asdfgh", "df"));
+		System.out.println(firstOccurence.strStr("asdfgh", "h"));
+		System.out.println(firstOccurence.strStr("asdfihdfgasssssdfihdfgasdfihdfgasssssidfihdfg", "gasssssi"));
+		System.out.println(firstOccurence.strStr("aaa", "aaaa"));
+		System.out.println(firstOccurence.strStr("aaa", "aaa"));
+		System.out.println(firstOccurence.strStr("mississippi", "issip"));
+		System.out.println(firstOccurence.strStr("mississippi", "issipi"));
+
+	}
+
+	public int strStr(String haystack, String needle) {
+		if(needle.length() > haystack.length())
+			return -1;
+		char[] charArray = haystack.toCharArray();
+
+		for (int i = 0; i < charArray.length; i++) {
+			if(i + needle.length() > charArray.length)
+				break;
+			int needleIndex = 0;
+
+			if (charArray[i] == needle.charAt(0)) {
+				boolean check = true;
+				while (check && needleIndex < needle.length()) {
+					check = charArray[i + needleIndex] == needle.charAt(needleIndex);
+					if (check) {
+						needleIndex++;
+					}
+				}
+				if (check) {
+					return i;
+				}
+			}
+
+		}
+		return -1;
+	}
+}
